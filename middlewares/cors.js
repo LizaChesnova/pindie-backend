9890e0f5755c95b173f7aae61767d0898a770a37
LizaChesnova <liza061004@icloud.com>
2024-05-24 18:28:58 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,6 @@
-const allowedCors = ["http://localhost:3000", "http://localhost:3001"];
+// Вместо нашего адреса вставь свой
+// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
+const allowedCors = ["https://pindiegamespanel.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
@@ -7,10 +9,7 @@ function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 }
 
